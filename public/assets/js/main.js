aea85218c246bfec4da9429ffacad5e81dfa3433
Alejandra Diaz <francys.diazurribarri2@gmail.com>
2022-04-08 09:28:31 +0200

    add handleClickSearch function,

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,24 @@
 'use strict';
+const buttonSearch = document.querySelector('.js-search-btn');
+const cocktailInput = document.querySelector('.js-cocktail-input');
 
-console.log('>> Ready :)');
+let drinks = [];
+function handleClicKSearch() {
+  const cocktail = cocktailInput.value;
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      drinks = data.drinks.map((coctel) => {
+        const newCoctel = {
+          strDrink: coctel.strDrink,
+          strDrinkThumb: coctel.strDrinkThumb,
+        };
+        return newCoctel;
+      });
+      console.log(drinks);
+    });
+}
+buttonSearch.addEventListener('click', handleClicKSearch);
 
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 'use strict';
+const buttonSearch = document.querySelector('.js-search-btn');
+const cocktailInput = document.querySelector('.js-cocktail-input');
 
-console.log('>> Ready :)');
+let drinks = [];
+function handleClicKSearch() {
+  const cocktail = cocktailInput.value;
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      drinks = data.drinks.map((coctel) => {
+        const newCoctel = {
+          strDrink: coctel.strDrink,
+          strDrinkThumb: coctel.strDrinkThumb,
+        };
+        return newCoctel;
+      });
+      console.log(drinks);
+    });
+}
+buttonSearch.addEventListener('click', handleClicKSearch);
